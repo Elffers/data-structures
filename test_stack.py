290c872d2faaa02e0b195d0d019ddfd3a3404a76
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-24 16:18:01 -0700

    order test passes

--- test_stack.py
+++ test_stack.py
@@ -25,6 +25,12 @@ class TestHStackFunctions(unittest.TestCase):
 	def test_pop_on_empty(self):
 		with self.assertRaises(IndexError):
 			self.stack.pop()
+	def test_pop_and_push_maintains_order(self):
+		self.stack.push(1)
+		self.stack.push(2)
+		self.stack.pop()
+		self.stack.push(3)
+		self.assertEqual(self.stack._contents, [1, 3])
 
 	def test_length(self):
 		self.stack.push(1)
@@ -35,5 +41,6 @@ class TestHStackFunctions(unittest.TestCase):
 		self.assertEqual(self.stack._max, 20)
 		self.assertEqual(self.stack10._max, 10)
 		
+# this is so you can run this as a python script independent of nosetests
 if __name__ == '__main__':
 	    unittest.main()
