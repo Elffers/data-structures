2de0a423dd27c9ae7dadafe1e9b15fa51df88607
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-03 16:25:49 -0700

    indentation fix

--- binary_tree.py
+++ binary_tree.py
@@ -1,62 +1,62 @@
 from hqueue import HQueue
 class BTree():
     def __init__(self):
-	self._root = None
+        self._root = None
 
     def push(self, value):
-	node = BTree.Node(value)
-	if not self._root:
-	    self._root = node
-	else:
-	    target = None
-	    nodes = self._breadth_traversal()._contents
-	    for	x in nodes:
-		if not x.complete():
-		    target = x
-		    break
-	    target.attach(node)
+        node = BTree.Node(value)
+        if not self._root:
+            self._root = node
+        else:
+            target = None
+            nodes = self._breadth_traversal()._contents
+            for	x in nodes:
+                if not x.complete():
+                    target = x
+                    break
+            target.attach(node)
 
     def breadth_first_traversal(self):
-	""" returns values of nodes returned breadth-first """
-	return [node.value for node in self._breadth_traversal()._contents]
+        """ returns values of nodes returned breadth-first """
+        return [node.value for node in self._breadth_traversal()._contents]
 
     def _breadth_traversal(self):
-	""" returns queue containing Node objects """
-	queue = HQueue()
-	queue.push(self._root)
-	i = 0
-	while i < len(queue._contents):
-	    el = queue._contents[i]
-	    if not el.childless():
-		queue.push(el.left)
-		if el.right:
-		    queue.push(el.right)
-	    i += 1
-	return queue
+        """ returns queue containing Node objects """
+        queue = HQueue()
+        queue.push(self._root)
+        i = 0
+        while i < len(queue._contents):
+            el = queue._contents[i]
+            if not el.childless():
+                queue.push(el.left)
+                if el.right:
+                    queue.push(el.right)
+            i += 1
+        return queue
 
     class Node():
-	def __init__(self, value):
-	    self.value = value
-	    self.index = None
-	    self.left = None
-	    self.right = None
-	
-	def complete(self):
-	    return self.right is not None and self.left is not None
+        def __init__(self, value):
+            self.value = value
+            self.index = None
+            self.left = None
+            self.right = None
 
-	def childless(self):
-	    return self.right is None and self.left is None
-	
-	def children(self):
-	    children = []
-	    if not self.childless():
-		children.append(self.left.value)
-		if self.right:
-		    children.append(self.right.value)
-	    return children
+        def complete(self):
+            return self.right is not None and self.left is not None
 
-	def attach(self, node):
-	   if self.childless():
-	       self.left = node
-	   else:
-	       self.right = node
+        def childless(self):
+            return self.right is None and self.left is None
+
+        def children(self):
+            children = []
+            if not self.childless():
+                children.append(self.left.value)
+                if self.right:
+                    children.append(self.right.value)
+            return children
+
+        def attach(self, node):
+            if self.childless():
+                self.left = node
+           else:
+               self.right = node
--- test_binary_tree.py
+++ test_binary_tree.py
@@ -3,73 +3,73 @@ from binary_tree import BTree
 
 class TestBtree(unittest.TestCase):
     def setUp(self):
-	self.btree = BTree()
-	self.node = BTree.Node(1)
+        self.btree = BTree()
+        self.node = BTree.Node(1)
 
     def test_node_complete(self):
-	self.node.left = BTree.Node(2)
-	self.node.right = BTree.Node(3)
-	self.assertEqual(self.node.complete(), True)
+        self.node.left = BTree.Node(2)
+        self.node.right = BTree.Node(3)
+        self.assertEqual(self.node.complete(), True)
 
     def test_node_incomplete(self):
-	self.node.left = BTree.Node(2)
-	self.assertEqual(self.node.complete(), False)
+        self.node.left = BTree.Node(2)
+        self.assertEqual(self.node.complete(), False)
 
     def test_set_root(self):
-	self.btree.push(10)
-	self.assertEqual(self.btree._root.value, 10)
+        self.btree.push(10)
+        self.assertEqual(self.btree._root.value, 10)
 #	self.assertEqual(self.btree._root.index, 0)
 
     def test_push_on_leaf(self):
-	self.btree.push(self.node)
-	self.btree.push(2)
-	self.assertEqual(self.btree._root.left.value, 2)
+        self.btree.push(self.node)
+        self.btree.push(2)
+        self.assertEqual(self.btree._root.left.value, 2)
 #	self.assertEqual(self.btree._last.index, 1)
 
     def test_push_on_left_filled(self):
-	self.btree.push(self.node)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.assertEqual(self.btree._root.right.value, 3)
+        self.btree.push(self.node)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.assertEqual(self.btree._root.right.value, 3)
 #	self.assertEqual(self.btree._root.left.index, 1)
 #	self.assertEqual(self.btree._root.right.index, 2)
-    
+
     def test_push_on_complete(self):
-	self.btree.push(self.node)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.btree.push(4)
-	self.assertEqual(self.btree._root.left.left.value, 4)
-    
+        self.btree.push(self.node)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.btree.push(4)
+        self.assertEqual(self.btree._root.left.left.value, 4)
+
     def test_push_to_6(self):
-	self.btree.push(self.node)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.btree.push(4)
-	self.btree.push(5)
-	self.btree.push(6)
-	self.assertEqual(self.btree._root.left.right.value, 5)
-	self.assertEqual(self.btree._root.right.left.value, 6)
+        self.btree.push(self.node)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.btree.push(4)
+        self.btree.push(5)
+        self.btree.push(6)
+        self.assertEqual(self.btree._root.left.right.value, 5)
+        self.assertEqual(self.btree._root.right.left.value, 6)
 
     def test_breadth_first_traversal(self):
-	self.btree.push(1)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.btree.push(4)
-	self.btree.push(5)
-	self.assertEqual(self.btree.breadth_first_traversal(), [1, 2, 3, 4, 5])
+        self.btree.push(1)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.btree.push(4)
+        self.btree.push(5)
+        self.assertEqual(self.btree.breadth_first_traversal(), [1, 2, 3, 4, 5])
 
     def test_breadth_traversal(self):
-	self.btree.push(1)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.btree.push(4)
-	self.btree.push(5)
-	self.assertEqual(len(self.btree._breadth_traversal()._contents), 5)
+        self.btree.push(1)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.btree.push(4)
+        self.btree.push(5)
+        self.assertEqual(len(self.btree._breadth_traversal()._contents), 5)
 
     def test_node_has_children(self):
-	self.btree.push(1)
-	self.btree.push(2)
-	self.btree.push(3)
-	self.assertEqual(self.btree._root.children(), [2, 3])
+        self.btree.push(1)
+        self.btree.push(2)
+        self.btree.push(3)
+        self.assertEqual(self.btree._root.children(), [2, 3])
 
