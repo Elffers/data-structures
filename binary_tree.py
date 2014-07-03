fdcb5bb16eef314150f41532b624aff161f75e29
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-02 18:43:02 -0700

    refactor and cleanup

--- binary_tree.py
+++ binary_tree.py
@@ -1,26 +1,15 @@
 class BTree():
     def __init__(self):
 	self._root = None
-	self._last = None
 
     def push(self, value):
 	node = BTree.Node(value)
 	if not self._root:
 	    self._root = node
-	    node.index = 0
 	else:
 	   current = self._root
-	   if current.childless():
-	       current.left = node
-	       node.index = current.index + 1
-	       self._last = node
-	   elif current.left and not current.right:
-	       current.right = node
-	       node.index = current.left.index + 1
-	       self._last = node
-	   elif current.complete():
+	   if current.complete():
 	       current = current.left
-	    
 
     class Node():
 	def __init__(self, value):
@@ -34,3 +23,9 @@ class BTree():
 
 	def childless(self):
 	    return self.right is None and self.left is None
+
+	def attach(self, node):
+	   if self.childless():
+	       self.left = node
+	   else:
+	       self.right = node
--- test_binary_tree.py
+++ test_binary_tree.py
@@ -10,19 +10,21 @@ class TestBtree(unittest.TestCase):
 	self.node.left = BTree.Node(2)
 	self.node.right = BTree.Node(3)
 	self.assertEqual(self.node.complete(), True)
+
     def test_node_incomplete(self):
 	self.node.left = BTree.Node(2)
 	self.assertEqual(self.node.complete(), False)
+
     def test_set_root(self):
 	self.btree.push(10)
 	self.assertEqual(self.btree._root.value, 10)
-	self.assertEqual(self.btree._root.index, 0)
+#	self.assertEqual(self.btree._root.index, 0)
 
     def test_push_on_empty(self):
 	self.btree.push(self.node)
 	self.btree.push(2)
 	self.assertEqual(self.btree._root.left.value, 2)
-	self.assertEqual(self.btree._last.index, 1)
+#	self.assertEqual(self.btree._last.index, 1)
 
     def test_push_on_left_filled(self):
 	self.btree.push(self.node)
@@ -30,8 +32,8 @@ class TestBtree(unittest.TestCase):
 	self.btree.push(3)
 	self.btree.push(4)
 	self.assertEqual(self.btree._root.right.value, 3)
-	self.assertEqual(self.btree._root.left.index, 1)
-	self.assertEqual(self.btree._root.right.index, 2)
+#	self.assertEqual(self.btree._root.left.index, 1)
+#	self.assertEqual(self.btree._root.right.index, 2)
     
     def test_push_on_complete(self):
 	self.btree.push(self.node)
