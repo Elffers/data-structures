a5df476cc877d5eaa664550df58d8dea22184a37
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-15 21:31:02 -0700

    append on empty list works

--- c/linked_list.c
+++ c/linked_list.c
@@ -14,6 +14,7 @@ struct linked_list {
     struct node *tail;
     int size;
 };
+
 /* calloc used to clear the memory for the types contained in the struct */
 /* using calloc instead of malloc also makes it unnecessary to initialize
  * the head pointer, since it will already be pointing to null */
@@ -24,10 +25,22 @@ struct linked_list *linked_list_init(){
    return list;
 };
 
-int append(struct linked_list *list, int value);
+int append(struct linked_list *list, int value){
+    if (list->size == 0){
+        list->head = (struct node *)malloc(sizeof(struct node));
+        list->head->value = value;
+        list->head->next = NULL;
+        list->tail = list->head;
+        return 0;
+    }
+    return 0;
+}
 
 int main(){
     struct linked_list *list;
     list = linked_list_init();
     printf("The size of the list is %d\n", list->size);
+    append(list, 1);
+    printf("The first value of the list is %d\n", list->head->value);
+    printf("The  value of the list's tail is %d\n", list->tail->value);
 }
