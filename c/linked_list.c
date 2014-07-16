431449b6fc605b30f673509744bcd9fb3507f9b5
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-15 23:11:42 -0700

    inspect list works

--- c/linked_list.c
+++ c/linked_list.c
@@ -35,23 +35,92 @@ int prepend(struct linked_list *list, int value){
     else {
         struct node *node_p = (struct node *)malloc(sizeof(struct node));
         node_p->value = value;
-        struct node *next = list->head;
-        node_p->next = next;
+        node_p->next = list->head;
         list->head = node_p;
     }
     list->size++;
     return 0;
 }
 
+int append(struct linked_list *list, int value){
+    if (list->size == 0){
+        list->head = (struct node *)malloc(sizeof(struct node));
+        list->head->value = value;
+        list->head->next = NULL;
+        list->tail = list->head;
+    }
+    else{
+        struct node *node_p = (struct node *)malloc(sizeof(struct node));
+        node_p->value = value;
+        node_p->next = NULL;
+        list->tail->next = node_p;
+        list->tail = node_p;
+    }
+    list->size++;
+    return 0;
+}
+/* this requires an arbirary int size to be declared first, such that 
+ * the pointer to that integer can be passed in as an argument to 
+ * the contents_p function */
+
+int *contents_p(struct linked_list *list, int *size_p){
+    *size_p = list->size;
+    int *output = (int *)malloc(sizeof(int)*list->size);
+    struct node *current;
+    int i;
+    i = 0;
+    current = list->head;
+    while(current){
+        output[i] = current->value;
+        i++;
+        current=current->next;
+    }
+    return output;
+}
+
+void inspect(struct linked_list *list){
+    int size;
+    int *contents = contents_p(list, &size);
+    int i;
+    i = 0;
+    printf("[");
+    while(i < size){
+        if (i == size-1){
+            printf("%d",contents[i]);
+        }
+        else{
+            printf("%d,",contents[i]);
+        }
+        i++;
+    }
+    printf("]\n");
+}
+
 int main(){
     struct linked_list *list;
     list = linked_list_init();
-    printf("The size of the list is %d\n", list->size);
+    printf("The size of the empty list is %d\n", list->size);
+    inspect(list);
+    printf("----------\n");
     prepend(list, 1);
-    printf("The first value of the list is %d\n", list->head->value);
+    printf("The first value after prepending once to the list is %d\n", list->head->value);
     printf("The  value of the list's tail is %d\n", list->tail->value);
     printf("The size of the list is %d\n", list->size);
+    inspect(list);
+    printf("----------\n");
     prepend(list, 2);
     printf("The  value of the list's head is %d\n", list->head->value);
+    printf("The size of the list after prepending twice is %d\n", list->size);
+    inspect(list);
+    printf("----------\n");
+    append(list, -1);
+    printf("The  value of the list's tail is %d\n", list->tail->value);
     printf("The size of the list is %d\n", list->size);
+    inspect(list);
+    printf("----------\n");
+    append(list, -2);
+    printf("The  value of the list's tail is %d\n", list->tail->value);
+    printf("The size of the list is %d\n", list->size);
+    inspect(list);
+    printf("----------\n");
 }
