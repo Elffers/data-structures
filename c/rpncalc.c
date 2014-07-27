fd5fd6af1f979fc042ca3fc89d4a09f0f42ca973
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-26 22:24:03 -0700

    switch case for major 4 operators

--- c/rpncalc.c
+++ c/rpncalc.c
@@ -5,7 +5,7 @@
 #include "array_stack.h"
 /* Reverse Polish Notation Calculator */
 
-void init(struct stack *s){
+void rpncalc_init(struct stack *s){
     s->length = 0;
     s->size = CHUNK;
     s->contents = (int *)malloc(sizeof(int) *s->size);
@@ -31,16 +31,47 @@ int eval(struct stack *s, char *expression){
     while(token != NULL){
         char *remainder;
         long num;
+
+        /* strtol ("string to long") converts as much of the string into
+         * a long int until it  hits a non-numeric character or NULL.
+         * The remainder is then set to point to that non-num character */
+
         num = strtol(token, &remainder, 10);
         printf("This is the token: %s\n", token);
+
+        /* this block gets hit at the last strtol iteration, bc the preceding
+         * strtok iteration would have returned the "operator + NULL" string,
+         * which would be assigned to remainder since operator is non-numeric*/
         if(remainder == token){
+            int operator2 = pop(s);
+            int operator1= pop(s);
+            int value;
+            switch (*token){
+                case '+':
+                    value = operator1 + operator2;
+                    break;
+                case '-':
+                    value = operator1 - operator2;
+                    break;
+                case '*':
+                    value = operator1 * operator2;
+                    break;
+                case '/':
+                    value = operator1 / operator2;
+                    break;
+            }
+            printf("value: %d\n", value);
             printf("This is an operator: %s\n", token);
         }
         else{
+            push(s, num);
             printf("Number: %ld\n", num);
         }
-        printf("remaining input:  %s\n", remainder);
         printf("---\n");
+        /* strtok will continue to grab bits of the original expression
+         * up until the next delimiter, which in this case is a single space.
+         * This effectively means that the token will always be a short
+         * string that is either number + NULL or operator + NULL */
         token = strtok(NULL, delim);
     }
 
@@ -50,8 +81,14 @@ int eval(struct stack *s, char *expression){
 int main(){
     struct stack input;
     struct stack *input_p = &input;
-    init(input_p);
-    char *expression = "1 2 +";
-    eval(input_p, expression);
+    rpncalc_init(input_p);
+    char *expression1 = "1 2 +";
+    eval(input_p, expression1);
+    char *expression2 = "2 3 *";
+    eval(input_p, expression2);
+    char *expression3 = "2 3 -";
+    eval(input_p, expression3);
+    char *expression4 = "6 3 /";
+    eval(input_p, expression4);
     return 0;
 }
